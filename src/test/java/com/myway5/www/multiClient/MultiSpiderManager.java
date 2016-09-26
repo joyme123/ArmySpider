5e4cd1a0c54cff86b6df8785f371a6fc92738f31
jiang <jiangpengfei12@gmail.com>
2016-09-26 17:19:21 +0800

    没什么内容

--- src/test/java/com/myway5/www/multiClient/MultiSpiderManager.java
+++ src/test/java/com/myway5/www/multiClient/MultiSpiderManager.java
@@ -1,5 +1,9 @@
 package com.myway5.www.multiClient;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import com.myway5.www.Pool.MultiFilterPagePool;
 import com.myway5.www.SpiderManager.AbstSpiderManager;
 import com.myway5.www.ThreadPool.FilterSpiderThreadPool;
@@ -16,6 +20,8 @@ public class MultiSpiderManager extends AbstSpiderManager{
 	}
 	
 	public static void main(String[] args){
+		Date date = Calendar.getInstance().getTime();
+		long start = Calendar.getInstance().getTimeInMillis();
 		boolean http = true,process = true,first = true,second = true;
 		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
 		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
@@ -45,6 +51,12 @@ public class MultiSpiderManager extends AbstSpiderManager{
 			firstFilterSpiderThreadPool.startMultiExecute();
 			secondFilterSpiderThreadPool.startMultiExecute();
 		}
+		Date date2 = Calendar.getInstance().getTime();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd HH:ii:ss");
+		long stop = Calendar.getInstance().getTimeInMillis();
+		System.out.println("开始时间:" + format.format(date));
+		System.out.println("终止时间:" + format.format(date2));
+		System.out.println("总耗时:" + (stop - start));
 	}
 
 }
